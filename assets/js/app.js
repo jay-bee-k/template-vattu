0afe1343410ba43a0cf672e3d16d3eeae5efe2f4
Jaybee-k <79182784+JayBee-K@users.noreply.github.com>
2022-07-26 22:23:47 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,38 +65,41 @@
         }
     }
 
-    let initSliderPromotion = function () {
-        if ($('#sliderPromotion .swiper').length) {
-            new Swiper('#sliderPromotion .swiper', {
-                speed: 1500,
-                spaceBetween: 10,
-                autoplay: {
-                    delay: 8000,
-                    disableOnInteraction: false,
-                },
-                lazy: {
-                    loadOnTransitionStart: true
-                },
-                preloadImages: true,
-                loop: true,
-                navigation: {
-                    nextEl: '#sliderPromotion .button-next',
-                    prevEl: '#sliderPromotion .button-prev',
-                },
-                breakpoints: {
-                    320: {
-                        slidesPerView: 1,
+    let initMultiSliderBanner = function () {
+        if ($('.slider-banner').length) {
+            $('.slider-banner').each(function () {
+                let sliderBannerID = $(this).attr('id');
+                new Swiper(`#${sliderBannerID} .swiper`, {
+                    speed: 1500,
+                    spaceBetween: 10,
+                    autoplay: {
+                        delay: 8000,
+                        disableOnInteraction: false,
                     },
-                    375: {
-                        slidesPerView: 1.5,
+                    lazy: {
+                        loadOnTransitionStart: true
                     },
-                    991: {
-                        slidesPerView: 2,
+                    preloadImages: true,
+                    loop: true,
+                    navigation: {
+                        nextEl: `#${sliderBannerID} .button-next`,
+                        prevEl: `#${sliderBannerID} .button-prev`,
                     },
-                    1199: {
-                        slidesPerView: 3,
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 1,
+                        },
+                        375: {
+                            slidesPerView: 1.5,
+                        },
+                        991: {
+                            slidesPerView: 2,
+                        },
+                        1199: {
+                            slidesPerView: 3,
+                        }
                     }
-                }
+                });
             });
         }
     }
@@ -140,6 +143,100 @@
         }
     }
 
+    let initSliderBrand = function () {
+        if ($('#sliderBrand').length) {
+            new Swiper(`#sliderBrand .swiper`, {
+                speed: 1500,
+                spaceBetween: 10,
+                autoplay: {
+                    delay: 8000,
+                    disableOnInteraction: false,
+                },
+                lazy: {
+                    loadOnTransitionStart: true
+                },
+                preloadImages: true,
+                loop: true,
+                navigation: {
+                    nextEl: `#sliderBrand .button-next`,
+                    prevEl: `#sliderBrand .button-prev`,
+                },
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                    },
+                    375: {
+                        slidesPerView: 1.5,
+                    },
+                    991: {
+                        slidesPerView: 2,
+                    },
+                    1199: {
+                        slidesPerView: 4,
+                    }
+                }
+            });
+        }
+    }
+
+    let [mediaThumb, mediaAvatar] = [];
+    let initSliderMedia = function () {
+        mediaThumb = new Swiper('#sliderMediaThumb .swiper', {
+            spaceBetween: 4,
+            slidesPerView: 3,
+            lazy: {
+                loadOnTransitionStart: true
+            },
+            preloadImages: true,
+            breakpoints: {
+                320: {
+                    slidesPerView: 2.75,
+                },
+                525: {
+                    slidesPerView: 3.4,
+                },
+                1199: {
+                    slidesPerView: 4,
+                },
+            },
+        });
+
+        mediaAvatar = new Swiper('#sliderMediaAvatar .swiper', {
+            thumbs: {
+                swiper: mediaThumb,
+            },
+            slidesPerView: 1,
+            lazy: {
+                loadOnTransitionStart: true
+            },
+            preloadImages: true,
+            navigation: {
+                nextEl: '#sliderMedia .button-next',
+                prevEl: '#sliderMedia .button-prev',
+            },
+        });
+
+        handleZoomSliderMedia($('#sliderMedia [data-fancybox=slider-avaThumb]'), mediaAvatar, mediaThumb);
+    }
+
+    let handleZoomSliderMedia = function (elm, mediaAvatar, mediaThumb) {
+        let i = 0;
+        elm.click(function () {
+            i = 0;
+        });
+
+        elm.fancybox({
+            touch: true,
+            loop: true,
+            beforeShow: function (instance, current) {
+                let index = $(`#sliderMedia [data-fancybox='slider-avaThumb'][href='${current.src}']`).attr('data-index');
+                mediaAvatar.slideTo(index);
+                mediaThumb.slideTo(index);
+            },
+        });
+    }
+
+
     let handleLimitCategories = function () {
         $('#more-categories').click(function () {
             if ($(this).closest('.categories-wrap').hasClass('no-limit')) {
@@ -157,9 +254,10 @@
 
         initSliderHeader();
         initSliderHero();
-        initSliderPromotion();
+        initMultiSliderBanner();
         initMultiSliderProduct();
-
+        initSliderBrand();
+        initSliderMedia();
         handleLimitCategories();
     });
 
